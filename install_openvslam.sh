1ce0277bb8f5072f2db5a38cc0cd4886cfbc0b15
aaasdream <60680961+aaasdream@users.noreply.github.com>
2020-08-19 15:00:00 +0800

    Update install_openvslam.sh

--- install_openvslam.sh
+++ install_openvslam.sh
@@ -137,3 +137,29 @@ cmake \
     -DBUILD_TESTS=OFF \
     ..
 make -j2
+
+
+echo "Download and run Demo"
+cd ~/openvslam_pkg/openvslam/build
+# download an ORB vocabulary from Google Drive
+FILE_ID="1wUPb328th8bUqhOk-i8xllt5mgRW4n84"
+curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=${FILE_ID}" > /dev/null
+CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
+curl -sLb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=${FILE_ID}" -o orb_vocab.zip
+unzip orb_vocab.zip
+
+# download a sample dataset from Google Drive
+FILE_ID="1d8kADKWBptEqTF7jEVhKatBEdN7g0ikY"
+curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=${FILE_ID}" > /dev/null
+CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
+curl -sLb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=${FILE_ID}" -o aist_living_lab_1.zip
+unzip aist_living_lab_1.zip
+
+
+# run tracking and mapping
+./run_video_slam -v ./orb_vocab/orb_vocab.dbow2 -m ./aist_living_lab_1/video.mp4 -c ./aist_living_lab_1/config.yaml --frame-skip 3 --no-sleep --map-db map.msg
+# click the [Terminate] button to close the viewer
+# you can find map.msg in the current directory
+
+
+

461b9f293b3023ecd697880662a8be74ad2088a1
aaasdream <60680961+aaasdream@users.noreply.github.com>
2020-08-19 22:13:20 +0800

    Create install_ros.sh

---
+++ install_ros.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# 這是自動安裝ROS Ubuntu 18 ，ROS Melodic
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+curl -sSL 'http://keyserver.ubuntu.com/pks/lookup?op=get&search=0xC1CF6E31E6BADE8868B172B4F42ED6FBAB17C654' | sudo apt-key add -
+sudo apt update
+sudo apt install ros-melodic-desktop-full
+#樹梅派sudo apt install ros-melodic-desktop
+echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
+source /opt/ros/melodic/setup.bash
+sudo apt install python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential
+sudo apt install python-rosdep
+sudo rosdep init
+rosdep update
+
+#建立工作資料夾
+mkdir -p ~/catkin_ws/src
+cd ~/catkin_ws/src
+catkin_init_workspace
+cd ~/catkin_ws/
+catkin_make
+source devel/setup.bash
+echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
+
+
+sudo apt-get install ros-kinetic-gazebo-ros-pkgs ros-kinetic-gazebo-ros-control
+#啟動
+gazebo
+
+#如果ubount 18 vmware中黑屏幕執行下面指令
+#export SVGA_VGPU10=0
+#echo "export SVGA_VGPU10=0" >> ~/.bashrc

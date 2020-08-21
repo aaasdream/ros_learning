26f5d0d51ba59b214682777463e1429e40df9feb
aaasdream <60680961+aaasdream@users.noreply.github.com>
2020-08-21 13:31:43 +0800

    Update install_openvslam.sh

--- install_openvslam.sh
+++ install_openvslam.sh
@@ -140,6 +140,33 @@ cmake \
 make -j2
 
 
+
+echo "ROS Package"
+sudo apt install -y ros-${ROS_DISTRO}-image-transport
+
+mkdir ~/catkin_openvslam_ws
+cd ~/catkin_openvslam_ws
+git clone --branch ${ROS_DISTRO} --depth 1 https://github.com/ros-perception/vision_opencv.git
+cp -r vision_opencv/cv_bridge src/
+rm -rf vision_opencv
+
+cd ~/catkin_openvslam_ws
+
+#PangolinViewer
+catkin_make \
+    -DBUILD_WITH_MARCH_NATIVE=ON \
+    -DUSE_PANGOLIN_VIEWER=ON \
+    -DUSE_SOCKET_PUBLISHER=OFF \
+    -DUSE_STACK_TRACE_LOGGER=ON \
+    -DBOW_FRAMEWORK=DBoW2
+
+echo "source ~/catkin_openvslam_ws/devel/setup.bash" >> ~/.bashrc
+source ~/catkin_openvslam_ws/devel/setup.bash
+
+
+
+
+
 echo "Download and run Demo"
 cd ~/openvslam_pkg/openvslam/build
 # download an ORB vocabulary from Google Drive

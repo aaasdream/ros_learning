4549812ea9a307a6b9fc89555aa1f086944366e7
aaasdream <60680961+aaasdream@users.noreply.github.com>
2020-08-21 09:32:44 +0800

    Update install_ros.sh

--- install_ros.sh
+++ install_ros.sh
@@ -4,11 +4,11 @@ sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 curl -sSL 'http://keyserver.ubuntu.com/pks/lookup?op=get&search=0xC1CF6E31E6BADE8868B172B4F42ED6FBAB17C654' | sudo apt-key add -
 sudo apt update
-sudo apt install ros-melodic-desktop-full
+sudo apt install -y ros-melodic-desktop-full
 #樹梅派sudo apt install ros-melodic-desktop
 echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
 source /opt/ros/melodic/setup.bash
-sudo apt install python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential
+sudo apt install -y python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential
 sudo rosdep init
 rosdep update
 
@@ -22,10 +22,13 @@ source devel/setup.bash
 echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
 
 
-sudo apt-get install ros-melodic-gazebo-ros-pkgs ros-melodic-gazebo-ros-control
-#啟動
-gazebo
+sudo apt-get install -y ros-melodic-gazebo-ros-pkgs ros-melodic-gazebo-ros-control
 
 #如果ubount 18 vmware中黑屏幕執行下面指令
 #export SVGA_VGPU10=0
 #echo "export SVGA_VGPU10=0" >> ~/.bashrc
+
+#啟動
+gazebo
+
+
